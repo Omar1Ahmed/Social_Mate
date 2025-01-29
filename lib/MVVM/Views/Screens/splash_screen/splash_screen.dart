3cb0d19f92027cca6bd154fa2b030fa74ca7e1e3
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-01-29 15:32:09 +0200

    adding flutter native splash screen

--- lib/MVVM/Views/Screens/splash_screen/splash_screen.dart
+++ lib/MVVM/Views/Screens/splash_screen/splash_screen.dart
@@ -1,57 +1,29 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:social_media/MVVM/Views/Screens/onBoarding_screen/onboarding_screen.dart';
-import 'package:social_media/Responsive/ui_component/info_widget.dart';
-import 'package:social_media/helper/extantions.dart';
 import 'package:social_media/routing/routs.dart';
 
-import '../../../../theming/colors.dart';
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 3), () {
-      context.pushReplacementNamed(Routes.onBordingScreen);
-    });
-  }
-
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return InfoWidget(builder: (context, info) {
-      return Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: ColorsManager.gradientColors,
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/splash_screen.png", height: 350),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 90),
-                  //assets/images/fullLogo.png
-                  child: Image.asset(
-                    "assets/images/fullLogo.png",
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, Routes.onBordingScreen);
     });
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Image.asset("assets/images/splash_screen.png", height: 350),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 90),
+              child: Image.asset("assets/images/fullLogo.png"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
