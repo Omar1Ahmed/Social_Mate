9073fc52141be2f3ca8e1098c61b166f15ecc75f
Abdullah-T3 <eng.abdullahahmed59@gmail.com>
2025-01-31 22:40:54 +0200

    Update appRouting.dart

--- lib/core/routing/appRouting.dart
+++ lib/core/routing/appRouting.dart
@@ -4,16 +4,20 @@ import 'package:social_media/features/authentication/presentation/ui/auth_screen
 import 'package:social_media/features/on_boarding/presentation/ui/onboarding_screen.dart';
 import 'package:social_media/features/splash_screen/presentation/ui/splash_screen.dart';
 
+import '../../features/home/presentation/homePage/ui/homePage_view.dart';
+
 class AppRouts {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.splashScreen:
         return MaterialPageRoute(builder: (context) => SplashScreen());
-          
+
       case Routes.onBordingScreen:
         return MaterialPageRoute(builder: (context) => OnboardingScreen());
       case Routes.loginScreen:
         return MaterialPageRoute(builder: (context) => SignInForm());
+      case Routes.homePage:
+        return MaterialPageRoute(builder: (context) => HomepageView());
       default:
         return null;
     }
