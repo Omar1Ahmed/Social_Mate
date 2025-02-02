be93d03eb1d831485f3fd94c8a7211896acdb1e1
Abdullah-T3 <eng.abdullahahmed59@gmail.com>
2025-02-02 23:39:29 +0200

    adding cubit to handle post card state

--- lib/features/posts/presentation/homePage/ui/widgets/post_card_widget.dart
+++ lib/features/posts/presentation/homePage/ui/widgets/post_card_widget.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_media/core/helper/extantions.dart';
 import 'package:social_media/core/routing/routs.dart';
 import '../../../../../../core/Responsive/Models/device_info.dart';
 import '../../../../../../core/theming/colors.dart';
 import '../../../../../../core/theming/styles.dart';
+import '../../logic/cubit/post_card_cubit.dart';
 
-class PostCardWidget extends StatefulWidget {
-  const PostCardWidget({super.key, required this.deviceInfo, required this.title, required this.content, required this.author, required this.timeAgo});
+class PostCardWidget extends StatelessWidget {
+  const PostCardWidget({
+    super.key,
+    required this.deviceInfo,
+    required this.title,
+    required this.content,
+    required this.author,
+    required this.timeAgo,
+  });
+
   final DeviceInfo deviceInfo;
   final String title;
   final String content;
@@ -14,117 +24,113 @@ class PostCardWidget extends StatefulWidget {
   final String timeAgo;
 
   @override
-  State<PostCardWidget> createState() => _PostCardWidgetState();
-}
-
-class _PostCardWidgetState extends State<PostCardWidget> {
-  bool _isExpanded = true; 
-
-  @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: Container(
-        width: widget.deviceInfo.localWidth * 0.9,
-        height: _isExpanded ? widget.deviceInfo.localHeight * 0.31 : widget.deviceInfo.localHeight * 0.37,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.black.withOpacity(0.5)),
-          borderRadius: BorderRadius.circular(widget.deviceInfo.localWidth * 0.03),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(widget.deviceInfo.localWidth * 0.05),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.title,
-                style: TextStyles.inter18Bold.copyWith(fontSize: widget.deviceInfo.screenWidth * 0.06),
+    return BlocProvider(
+      create: (_) => PostCardCubit(),
+      child: BlocBuilder<PostCardCubit, bool>(
+        builder: (context, isExpanded) {
+          return InkWell(
+            child: Container(
+              width: deviceInfo.localWidth * 0.9,
+              height: isExpanded ? deviceInfo.localHeight * 0.31 : deviceInfo.localHeight * 0.37,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.black.withOpacity(0.5)),
+                borderRadius: BorderRadius.circular(deviceInfo.localWidth * 0.03),
               ),
-              Divider(color: Colors.black, thickness: 1.5),
-              Row(
-                children: [
-                  Image.asset(
-                    'assets/images/man_profile.png',
-                    width: widget.deviceInfo.localWidth * 0.1,
-                    height: widget.deviceInfo.localHeight * 0.1,
-                  ),
-                  SizedBox(width: widget.deviceInfo.localWidth * 0.02),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.author,
-                        style: TextStyles.inter18BoldBlack.copyWith(fontSize: widget.deviceInfo.screenWidth * 0.04),
-                      ),
-                      Text(
-                        widget.timeAgo.substring(0, 10),
-                        style: TextStyles.inter18Regularblack.copyWith(fontSize: widget.deviceInfo.screenWidth * 0.03),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Report',
-                      style: TextStyle(color: ColorsManager.redColor, fontSize: widget.deviceInfo.screenWidth * 0.036),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: widget.deviceInfo.localHeight * 0.01),
-              Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(deviceInfo.localWidth * 0.05),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      widget.content,
-                      maxLines: _isExpanded ? null : 3,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.justify,
-                      softWrap: true,
-                      style: TextStyles.inter18Regular.copyWith(fontSize: widget.deviceInfo.screenWidth * 0.04),
+                      title,
+                      style: TextStyles.inter18Bold.copyWith(fontSize: deviceInfo.screenWidth * 0.06),
                     ),
-                    if (_isExpanded)
-                      TextButton(
-                        onPressed: () {
-                          setState(() {
-                            _isExpanded = false;
-                          });
-                        },
-                        child: Text(
-                          'See More',
-                          style: TextStyle(
-                            color: ColorsManager.primaryColor,
-                            fontSize: widget.deviceInfo.screenWidth * 0.04,
+                    Divider(color: Colors.black, thickness: 1.5),
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/man_profile.png',
+                          width: deviceInfo.localWidth * 0.1,
+                          height: deviceInfo.localHeight * 0.1,
+                        ),
+                        SizedBox(width: deviceInfo.localWidth * 0.02),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              author,
+                              style: TextStyles.inter18BoldBlack.copyWith(fontSize: deviceInfo.screenWidth * 0.04),
+                            ),
+                            Text(
+                              timeAgo.substring(0, 10),
+                              style: TextStyles.inter18Regularblack.copyWith(fontSize: deviceInfo.screenWidth * 0.03),
+                            ),
+                          ],
+                        ),
+                        const Spacer(),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Report',
+                            style: TextStyle(color: ColorsManager.redColor, fontSize: deviceInfo.screenWidth * 0.036),
                           ),
                         ),
-                      )
-                    else
-                      TextButton(
-                        onPressed: () {
-                          setState(() {
-                            _isExpanded = true;
-                          });
-                        },
-                        child: Text(
-                          'Show Less',
-                          style: TextStyle(
-                            color: ColorsManager.primaryColor,
-                            fontSize: widget.deviceInfo.screenWidth * 0.04,
+                      ],
+                    ),
+                    SizedBox(height: deviceInfo.localHeight * 0.01),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            content,
+                            maxLines: isExpanded ? null : 3,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.justify,
+                            softWrap: true,
+                            style: TextStyles.inter18Regular.copyWith(fontSize: deviceInfo.screenWidth * 0.04),
                           ),
-                        ),
+                          if (isExpanded)
+                            TextButton(
+                              onPressed: () {
+                                context.read<PostCardCubit>().toggleExpansion();
+                              },
+                              child: Text(
+                                'See More',
+                                style: TextStyle(
+                                  color: ColorsManager.primaryColor,
+                                  fontSize: deviceInfo.screenWidth * 0.04,
+                                ),
+                              ),
+                            )
+                          else
+                            TextButton(
+                              onPressed: () {
+                                context.read<PostCardCubit>().toggleExpansion();
+                              },
+                              child: Text(
+                                'Show Less',
+                                style: TextStyle(
+                                  color: ColorsManager.primaryColor,
+                                  fontSize: deviceInfo.screenWidth * 0.04,
+                                ),
+                              ),
+                            ),
+                        ],
                       ),
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+            onTap: () {
+              context.pushNamed(Routes.postDetailsScreen);
+            },
+          );
+        },
       ),
-      onTap: () {
-        context.pushNamed(Routes.postDetailsScreen);
-      },
     );
   }
 }
